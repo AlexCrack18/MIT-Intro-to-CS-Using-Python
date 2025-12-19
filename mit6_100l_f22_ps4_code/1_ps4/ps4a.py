cbfc33e54c44e2b6345f23bb9f81d2112b7ea04b
imnotalex <alexmagno180907@gmail.com>
2025-12-19 17:58:55 -0500

    started ps4

--- mit6_100l_f22_ps4_code/1_ps4/ps4a.py
+++ mit6_100l_f22_ps4_code/1_ps4/ps4a.py
@@ -7,9 +7,9 @@ from tree import Node # Imports the Node object used to construct trees
 # Part A0: Data representation
 # Fill out the following variables correctly.
 # If correct, the test named test_data_representation should pass.
-tree1 = None #TODO
-tree2 = None #TODO
-tree3 = None #TODO
+tree1 = Node(8, Node(2, Node(1), Node(6)), Node(10))
+tree2 = Node(7, Node(2, Node(1), Node(5, Node(3), Node(6))), Node(9, Node(8), Node(10)))
+tree3 = Node(5, Node(3, Node(2), Node(4)), Node(14, Node(12), Node(21, Node(20), Node(26))))
 
 def find_tree_height(tree):
     '''
@@ -19,8 +19,11 @@ def find_tree_height(tree):
     Output:
         The integer depth of the tree
     '''
-    # TODO: Remove pass and write your code here
-    pass
+
+    if tree is None:
+        return -1
+    return 1+ max(find_tree_height(tree.get_left_child()), find_tree_height(tree.get_right_child()))
+
 
 def is_heap(tree, compare_func):
     '''
@@ -33,12 +36,29 @@ def is_heap(tree, compare_func):
     Output:
         True if the entire tree satisfies the compare_func function; False otherwise
     '''
-    # TODO: Remove pass and write your code here
-    pass
+    if tree.get_left_child() is None and tree.get_right_child() is None:
+        return True
+    
+    left_is_heap, right_is_heap = True, True
+    
+    if tree.get_left_child() is not None:
+        left_is_heap = compare_func(tree.get_left_child().get_value(), tree.get_value())
+        
+    if tree.get_right_child() is not None:
+        right_is_heap = compare_func(tree.get_right_child().get_value(), tree.get_value())
+        
+    if left_is_heap and right_is_heap and is_heap(tree.get_right_child(), compare_func) and is_heap(tree.get_left_child(), compare_func):
+        return True 
+    return False
 
 
 
 if __name__ == '__main__':
     # You can use this part for your own testing and debugging purposes.
-    # IMPORTANT: Do not erase the pass statement below if you do not add your own code
+    # IMPORTANT: Do not erase the pass statement below if you do not add your own code]
+    def comp_func(child_value, parent_value):
+        if child_value > parent_value:
+            return True
+        return False
+    print(is_heap(tree2, comp_func))
     pass
